6ab88a78fcdcd998d33c9962febd2f6caa0fea99
jonasweckhuysen-droid <jonasweckhuysen@gmail.com>
2025-11-19 11:51:24 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,18 @@
 self.addEventListener('install', event => {
-  console.log('Service Worker installeren.');
+  console.log('Service Worker installed');
+  event.waitUntil(
+    caches.open('boekhouding-v1').then(cache => cache.addAll([
+      '/Boekhouding/index.html',
+      '/Boekhouding/main.js',
+      '/Boekhouding/styles.css',
+      '/Boekhouding/icons/icon-192.png',
+      '/Boekhouding/icons/icon-512.png'
+    ]))
+  );
 });
 
 self.addEventListener('fetch', event => {
-  // Optioneel: cache strategie voor offline gebruik
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
