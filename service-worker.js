4d419a9cf3fbd25a730b67dd60c8d720b7ac52c2
jonasweckhuysen-droid <jonasweckhuysen@gmail.com>
2025-12-28 20:26:04 +0100

    Update service worker to use new cache version and paths

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,35 @@
-const CACHE_NAME = "boekhouding-v1";
+const CACHE_NAME = "boekhouding-v2";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./overzicht.html",
-  "./manifest.json",
+  "/Boekhouding/",
+  "/Boekhouding/index.html",
+  "/Boekhouding/overzicht.html",
+  "/Boekhouding/style.css",
+  "/Boekhouding/script.js",
+  "/Boekhouding/manifest.json",
+  "/Boekhouding/icons/icon-192.png",
+  "/Boekhouding/icons/icon-512.png",
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
-      )
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
